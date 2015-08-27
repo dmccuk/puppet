9ea85f85b07ffd89f32c5be7885b6f976c42e774
Administrator <dmccuk@gmail.com>
2015-08-27 23:03:54 +0100

    adding lots of new shiny things...

--- environments/production/modules/build/httpd/manifests/init.pp
+++ environments/production/modules/build/httpd/manifests/init.pp
@@ -1,6 +1,18 @@
 class httpd {
 
-  notify { "This is the HTTPD module": }
+  #notify { "This is the HTTPD module": }
 
+  package{ 'httpd':
+    ensure => present,
+  }
+  file{ "/var/www/html/index.html":
+    ensure   => file,
+    content  => template("$module_name/index.html.erb"),
+  }
+  service{ 'httpd':
+    name => httpd,
+    ensure => running,
+    enable => true,
+  }
 }
 
