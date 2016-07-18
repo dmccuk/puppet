1b39a2baed0eacd2d9b2d34decdb9ed6cec19a2d
Dennis McCarthy <dmccuk@gmail.com>
2016-07-18 14:15:22 +0100

    updates manifest/site.pp

--- environments/development/manifests/site.pp
+++ environments/development/manifests/site.pp
@@ -1,18 +1,11 @@
-<<<<<<< HEAD
-#
-
-=======
->>>>>>> cad1b08d0a136f420ff5814d7d2870084ff4d47e
 Package {
   allow_virtual => false
 }
 
-<<<<<<< HEAD
 node default {
   include auto
 }
 
-=======
 hiera_include('classes')
 
 node "puppet-node02.mydomain.com", "test-node01.mydomain.com" {
@@ -21,4 +14,3 @@ node "puppet-node02.mydomain.com", "test-node01.mydomain.com" {
       # or:
       # class { "ntp": }
 }
->>>>>>> cad1b08d0a136f420ff5814d7d2870084ff4d47e
